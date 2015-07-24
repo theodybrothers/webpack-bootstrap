5022f37ea63f44d786fdf9a43d198a012f3911ea
Jason Ody <jasonody@users.noreply.github.com>
2015-07-23 19:31:28 -0700

    Webpack and Bootstrap's CSS only

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	entry: {
+		app: ["webpack/hot/dev-server", "./app/app.js"],
+	},
+	output: {	
+		path: './app',
+		filename: 'bundle.js'
+	},
+	module: {
+	  loaders: [
+			{ test: /\.jsx?$/, exclude: /(node_modules|bower_components)/, loader: 'babel' },
+			{ test: /\.css$/, loader: 'style-loader!css-loader' },
+			{ test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+			{ test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
+			{ test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+			{ test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
+	  ]
+	}
+};
